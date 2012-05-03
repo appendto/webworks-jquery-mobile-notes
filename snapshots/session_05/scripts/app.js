dc57b6fdefaf891ecfba64d7c2bb00a85ea93b06
Doug Neiner <doug@pixelgraphics.us>
2012-05-03 08:01:06 -0400

    Added session 5 snapshot

---
+++ snapshots/session_05/scripts/app.js
@@ -0,0 +1,40 @@
+(function( app, $ ){
+
+var $html = $( document.documentElement );
+
+
+$html.on( "pageinit", "#home", function () {
+	new app.NotesView({
+		collection: app.notes,
+		el: this
+	});
+});
+
+$html.on( "pageinit", "#new-note", function () {
+	new app.NewNoteView({
+		collection: app.notes,
+		el: this
+	});
+});
+
+$html.on( "pagebeforechange", function ( e, data ) {
+	if ( typeof data.toPage === "string" && !$.mobile.path.isEmbeddedPage( data.toPage ) ) {
+		var url = $.mobile.path.parseUrl( data.toPage );
+
+		if ( url.directory === "/notes/" ) {
+			var model = app.notes.get( url.filename ),
+				page = new app.NoteView({
+					model: model
+				});
+
+			page.$el.appendTo( document.body );
+
+			data.options.dataUrl = data.toPage;
+			data.toPage = page.$el;
+		}
+	}
+});
+
+app.notes.fetch();
+
+}( window.notesapp = window.notesapp || {}, jQuery ));
